e0555254a2a79062d747a575569a0566fb14b875
ferhatgec <ferhatgec@pop-os.localdomain>
2020-03-29 21:59:55 +0300

    Fegeya Fusion new Changes
    First,I'm changed HAIKU_TOP to FEGEYA_TOP
    
    and I'm added boolx header.

---
+++ src/preferences/appearance/boolx.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2020, Ferhat Gecdogan.
+ * Distributed under the terms of the GPLv3 License.
+ *
+ * Authors:
+ *		Ferhat Geçdoğan <ferhatgectao@gmail.com>
+ */
+#ifndef BOOLX_H
+#define BOOLX_H
+
+
+bool is_dark;
+
+#endif	// COLORS_H
